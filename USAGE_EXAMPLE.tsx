50febbf48bb6925f57b712d5337e0d43596ccb49
RK Zahid Hassan Dipu <89594765+rkzahidhassandipu@users.noreply.github.com>
2026-04-08 21:03:00 +0800

    Delete USAGE_EXAMPLE.tsx

--- USAGE_EXAMPLE.tsx
+++
@@ -1,9 +0,0 @@
-import AdminFoodManager from '@/components/modules/Admin/AdminFoodManager';
-
-export default function AdminFoodPage() {
-  return (
-    <div className="space-y-6">
-      <AdminFoodManager />
-    </div>
-  );
-}
