4b9c9aee7080b05f814e20a7d2ce418959457625
Raihan Uddin <rkrazzakhan01731@gmail.com>
2026-04-08 20:44:48 +0800

    admin dashboard room create ok but have category anemity and room updated

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qT9oeGcxDMbisg4glukiJ2HiH9yMGBmjswA7XW32Cag=\"\n}"
--- .next/dev/static/chunks/src_app_error_tsx_0bpd6-u._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_error_tsx_0g_t3kl._.js"],
-    source: "dynamic"
-});
